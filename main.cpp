468109996894833844087be592b1a611f1b0c4f3
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-11-24 22:43:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,31 @@
 using namespace std;
 
 int main() {
+    int n=0;
+    cin >> n;
+    int a=1, b=1, risultato=0;
+    if (n==1) {
+        cout << "1" << endl;
+        cout << "1" << endl;
+    } else if (n==2) {
+        cout << "1" << endl;
+        cout << "1" << endl;
+        cout << "2" << endl;
+    } else if (n==0) {
+        cout << "0" << endl;
+    } else {
+        cout << "1" << endl;
+        cout << "1" << endl;
+        for (int i = 0; i <=(n-3); ++i) {
+
+            risultato = a+b;
+            if (risultato>n) break;
+            cout << risultato << endl;
+            a = b;
+            b= risultato;
+
+
+        }
+    }
    return 0;
 }
